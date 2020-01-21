0c1e9d74f13185e4acd9106d462e13bdf79529db
s.orlandini <s.orlandini@cineca.it>
2020-01-21 10:49:07 +0100

    version option

--- src/presenzialo/presenzialo_args.py
+++ src/presenzialo/presenzialo_args.py
@@ -1,10 +1,22 @@
 import argparse
+from .presenzialo_config import version
 
 
 def add_parser_debug(parser):
 
-    parser.add_argument("--raw", action="store_true", help="raw data")
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="%(prog)s " + version,
+        help="print version information",
+    )
+
+    parser.add_argument(
+        "-v", "--verbose", action="count", default=0, help="increase verbosity"
+    )
 
     parser.add_argument(
         "--debug", "--dry-run", dest="dry_run", action="store_true", help="dry-run mode"
     )
+
+    parser.add_argument("--raw", action="store_true", help="raw data")
