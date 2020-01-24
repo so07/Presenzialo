d746809cd8d1ee492f5ac7c3779f948ec9364c36
s.orlandini <s.orlandini@cineca.it>
2020-01-24 12:25:42 +0100

    update to version 0.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="presenzialo",
-    version="0.3.5",
+    version="0.4.0",
     description="presence web manager",
     long_description="presence web manager",
     author="so07",
